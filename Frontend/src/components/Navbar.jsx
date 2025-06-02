135d66487575367ad10e4f5aadbee403fad5bf51
Hardik427 <gondwalhardik22@gmail.com>
2025-06-02 16:36:47 +0530

    Added Login Logout states

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CartModal from "../pages/Shop/CartModal";
+import avatarImg from '../assets/avatar.png'
+
 const Navbar = () => {
 
     const products = useSelector((state) => state.cart.products);
-    const [isCartOpen,setIsCartOpen] = useState(false);
+    const [isCartOpen, setIsCartOpen] = useState(false);
     const handleCartToggle = () => {
         setIsCartOpen(!isCartOpen);
     }
+
+    const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.auth)
+ 
+    const [isDropDownOpen,setIsDropDownOpen] = useState(false);
+    const handleDropDown = ()=>{
+        setIsDropDownOpen(!isDropDownOpen);
+    }
+
+
+
     return (
         <header className="fixed-nav-bar w-nav">
             <nav className="max-w-screen-2xl mx-auto px-4 flex justify-between items-center">
@@ -26,19 +39,27 @@ const Navbar = () => {
                     </Link></span>
                     <span>
                         <button onClick={handleCartToggle} className=" hover:text-primary">
-                        <i className="ri-shopping-bag-3-line"></i>
-                        <sup className=" text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center">{products.length}</sup>
+                            <i className="ri-shopping-bag-3-line"></i>
+                            <sup className=" text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center">{products.length}</sup>
                         </button>
                     </span>
                     <span>
-                        <Link to="/login">
+                        {
+                            user && user ? (
+                               <>
+                               <img src={user?.profileImage || avatarImg} alt="" className="size-6 rounded-full cursor-pointer" />
+                               </>
+                            ) : (
+                                <Link to="/login">
                         <i className="ri-user-fill"></i>
                         </Link>
+                            )
+                        }
                     </span>
                 </div>
             </nav>
             {
-                isCartOpen && <CartModal products={products} isOpen={isCartOpen} onClose={handleCartToggle}/>
+                isCartOpen && <CartModal products={products} isOpen={isCartOpen} onClose={handleCartToggle} />
             }
         </header>
     )
