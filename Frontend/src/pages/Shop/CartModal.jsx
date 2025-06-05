f3f286e5ff8a28ba0fde064ff04eaa4c6d1c0cc0
Hardik427 <gondwalhardik22@gmail.com>
2025-06-05 17:15:14 +0530

    minor changes

--- Frontend/src/pages/Shop/CartModal.jsx
+++ Frontend/src/pages/Shop/CartModal.jsx
@@ -17,7 +17,7 @@ const CartModal = ({ products, isOpen, onClose }) => {
     }
     return (
         <div className={`fixed z-[1000] inset-0 bg-black bg-opacity-80 transition-opacity ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
-            style={{ transition: 'opacity 300ms' }} x
+            style={{ transition: 'opacity 300ms' }} 
         >
             <div className={`fixed right-0 top-0 md:w-1/3 w-full bg-white h-full overflow-y-auto transition-transform ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}
                 style={{ transition: 'transform 300ms cubic-bezier(0.25,0.46,0.45,0.94' }}
