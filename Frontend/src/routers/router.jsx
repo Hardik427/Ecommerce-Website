d88413b592f501955b1ba0c10c7175c82fa991a3
Hardik427 <gondwalhardik22@gmail.com>
2025-06-11 15:59:33 +0530

    Created new routes for frontend

--- Frontend/src/routers/router.jsx
+++ Frontend/src/routers/router.jsx
@@ -7,27 +7,31 @@ import ShopPage from "../pages/Shop/ShopPage";
 import SingleProduct from "../pages/Shop/productDetails/SingleProduct";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import Contact from "../pages/Contact/Contact";
+import AdminDashboard from "../pages/Dashboard/AdminDashboard";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
-            {path: "/",element: <Homepage/>},
-            {path:"/categories/:categoryName", element:<CategoryPage/>},
-            {path:"/search", element:<Search/>},
-            {path:"/shop", element:<ShopPage/>},
-            {path:"/shop/:id" , element:<SingleProduct/>}
+            { path: "/", element: <Homepage /> },
+            { path: "/categories/:categoryName", element: <CategoryPage /> },
+            { path: "/search", element: <Search /> },
+            { path: "/shop", element: <ShopPage /> },
+            { path: "/shop/:id", element: <SingleProduct /> },
+            { path: "/contact", element: <Contact/> },
+            { path: "/dashboard/admin", element:<AdminDashboard/>}
         ]
     },
     {
         path: "/login",
-        element : <Login/>
+        element: <Login />
     },
     {
         path: "/register",
-        element : <Register/>
-    }
+        element: <Register />
+    },
 ]);
 
 export default router;
